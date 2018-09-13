90b7265fb6181f7dc22452fa992cccd010b75e6a
Shankaranarayanan Govindarajan <shgovind@shgovind-mac.home>
2018-09-12 21:32:06 -0500

    Init commit

---
+++ src/main/java/com/sample/metric/Application.java
@@ -0,0 +1,40 @@
+package com.sample.metric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application { 
+	
+	private int qDepth = 100;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping(value="/")
+	String getAppInfo() {
+		return "This is a Metric emulation app";
+	}
+	
+	@RequestMapping(value="/getdepth")
+	int getDepth() {
+		return qDepth;
+	}
+	
+	@RequestMapping(value="/setdepth")
+	String setDepth(@RequestParam int size) {
+		qDepth = size;
+		return "Queue Depth successfully set to " + size ;
+	}
+	
+	@RequestMapping(value="/resetdepth")
+	String resetDepth() {
+		qDepth = 100;
+		return "Queue Depth successfully reset to 100";
+	}
+}
